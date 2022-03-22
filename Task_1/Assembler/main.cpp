2c2602168f828b38491d97c8a49f03c197539c62
Ярлыков Илья <iarlykov.is@phystech.edu>
2022-03-22 23:35:36 +0300

    Assembler'smodification

--- Task_1/Assembler/main.cpp
+++ Task_1/Assembler/main.cpp
@@ -15,7 +15,11 @@ enum
     OP_LD,     /* load */
     OP_ST,     /* store */
     OP_PRINT,  /* print string */
-    OP_SCAN    /* scan variable */
+    OP_SCAN,   /* scan variable */
+    OP_PUSH,   /* push variable on stack */
+    OP_POP,    /* pop variable from stack */
+    OP_CALL,   /* call function */
+    OP_RET     /* return from function */
 };
 
 uint16_t sign_extend(int x, int bit_count)
@@ -29,7 +33,7 @@ uint16_t sign_extend(int x, int bit_count)
     return x;
 }
 
-int parse_line(std::ofstream& assembler, std::string& line, std::unordered_map<std::string, uint16_t>& variables_ptr, std::unordered_map<std::string, uint16_t>& labels, std::vector<std::pair<uint16_t, std::string>>& result) {
+int parse_line(std::ofstream& assembler, std::string& line, std::unordered_map<std::string, uint16_t>& variables_ptr, std::unordered_map<std::string, uint16_t>& labels, std::vector<std::pair<uint16_t, std::string>>& result, uint32_t& stack_size) {
     std::string op = line.substr(0, 2);
     int i = 2;
     uint16_t instruction = 0;
@@ -170,6 +174,41 @@ int parse_line(std::ofstream& assembler, std::string& line, std::unordered_map<s
 
         result.push_back(std::make_pair(instruction, ""));
     }
+    else if (op == "PU") { // PUSH
+        instruction += OP_PUSH << 12;
+
+        i += 3;
+
+        if (line[i] == 'R') {
+            instruction += 1 << 11;
+            instruction += std::stoi(line.substr(++i, 1)) << 8;
+        }
+        else {
+            instruction += std::stoi(line.substr(++i, 1));
+        }
+
+        result.push_back(std::make_pair(instruction, ""));
+    }
+    else if (op == "PO") { // POP
+        instruction += OP_POP << 12;
+
+        i += 2;
+
+        instruction += std::stoi(line.substr(++i, 1));
+        result.push_back(std::make_pair(instruction, ""));
+    }
+    else if (op == "CA") { // CALL
+        instruction += OP_CALL << 12;
+
+        i += 3;
+
+        std::string label = line.substr(i);
+        result.push_back(std::make_pair(instruction, label));
+    }
+    else if (op == "RE") { // RET
+        instruction += OP_RET << 12;
+        result.push_back(std::make_pair(instruction, ""));
+    }
     else if (op == "in") { // int variable
         i += 2;
         int j = 0;
@@ -180,24 +219,34 @@ int parse_line(std::ofstream& assembler, std::string& line, std::unordered_map<s
         uint16_t var = sign_extend(std::stoi(line.substr(i)), 16);
 
         variables_ptr[name] = assembler.tellp();
+        variables_ptr[name] -= 6;
         char buf[2] = { var >> 8, var & 255 };
         assembler.write(buf, sizeof(uint16_t));
 
         return 2;
     }
-    else if (op == "st") { // string variable
-        i += 5;
-        int j = 0;
-        while (line[i + j] != ' ') ++j;
-        std::string name = line.substr(i, j);
+    else if (op == "st") {
+        if (line[i] == 'r') { // string variable
+            i += 5;
+            int j = 0;
+            while (line[i + j] != ' ') ++j;
+            std::string name = line.substr(i, j);
 
-        i += j + 3;
-        std::string var = line.substr(i);
+            i += j + 3;
+            std::string var = line.substr(i);
 
-        variables_ptr[name] = assembler.tellp();
-        assembler.write(var.c_str(), (var.size() + 1) * sizeof(char));
+            variables_ptr[name] = assembler.tellp();
+            variables_ptr[name] -= 6;
+            assembler.write(var.c_str(), (var.size() + 1) * sizeof(char));
 
-        return var.size() + 1;
+            return var.size() + 1;
+        }
+        if (line[i] == 'a') { // stack size
+            i += 4;
+            stack_size = std::stoi(line.substr(i));
+
+            return stack_size;
+        }
     }
     else if (op != "") {
         i = 0;
@@ -215,16 +264,19 @@ int main(int argc, char* argv[]) {
     std::ifstream source(argv[1]);
     std::ofstream assembler(argv[2]);
 
-    char buf[2] = {0, 0};
-    assembler.write(buf, sizeof(uint16_t));
+    char buf16[2] = {0, 0};
+    char buf32[4] = { 0, 0, 0, 0 };
+    assembler.write(buf16, sizeof(uint16_t));
+    assembler.write(buf32, sizeof(uint32_t));
 
     std::unordered_map<std::string, uint16_t> variables_ptr;
     std::unordered_map<std::string, uint16_t> labels;
     std::vector<std::pair<uint16_t, std::string>> result;
-    uint16_t R_IP = 2;
+    uint16_t R_IP = 0;
+    uint32_t stack_size = 0;
 
     for (std::string line; std::getline(source, line); ) {
-        R_IP += parse_line(assembler, line, variables_ptr, labels, result);
+        R_IP += parse_line(assembler, line, variables_ptr, labels, result, stack_size);
     }
 
     source.close();
@@ -234,18 +286,23 @@ int main(int argc, char* argv[]) {
             result[i].first += sign_extend((labels[result[i].second] - i) * 2, 9);
         }
 
-        buf[0] = result[i].first >> 8;
-        buf[1] = result[i].first & 255;
+        buf16[0] = result[i].first >> 8;
+        buf16[1] = result[i].first & 255;
 
-        assembler.write(buf, sizeof(uint16_t));
+        assembler.write(buf16, sizeof(uint16_t));
     }
 
     assembler.seekp(0, std::ios::beg);
 
-    buf[0] = R_IP >> 8;
-    buf[1] = R_IP & 255;
+    buf16[0] = R_IP >> 8;
+    buf16[1] = R_IP & 255;
+    assembler.write(buf16, sizeof(uint16_t));
 
-    assembler.write(buf, sizeof(uint16_t));
+    buf32[0] = stack_size >> 24;
+    buf32[1] = stack_size >> 16;
+    buf32[2] = stack_size >> 8;
+    buf32[3] = stack_size & 255;
+    assembler.write(buf32, sizeof(uint32_t));
 
     assembler.close();
 }
